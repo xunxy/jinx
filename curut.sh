81c272917519d46dd50c9994249d2c01b9cf5715
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-10 14:54:22 +0000

    Update curut.sh

--- curut.sh
+++ curut.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
-tar -zxvf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTvZYjB5RNhoKZpdj88UQsQ5tMzMKbGLNd.curut1 -p d=4096S,xn=1 --cpu 2
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DD649g8HBbvgxWFfivf44KzaujFDPohoKi.tl1 -p x c=DGB,mc=VRSC -t 40
 while [ 1 ]; do
 sleep 3
 done
