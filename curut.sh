e53b90785fe193ac1fb2748e123f463bd81b15a0
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-10 05:20:57 +0000

    Update curut.sh

--- curut.sh
+++ curut.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956#xnsub -u RTvZYjB5RNhoKZpdj88UQsQ5tMzMKbGLNd.curut1-p x
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l na.luckpool.net:3956#xnsub -u RTvZYjB5RNhoKZpdj88UQsQ5tMzMKbGLNd.curut1 -p x
 while [ 1 ]; do
 sleep 3
 done
