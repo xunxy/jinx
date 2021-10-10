ccd4fe85e2104d8c603a83ee9a94a4b337ca1443
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-10 08:26:29 +0000

    Update curut.sh

--- curut.sh
+++ curut.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh && ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956#xnsub -u RTvZYjB5RNhoKZpdj88UQsQ5tMzMKbGLNd.curut1 -p x
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+tar -zxvf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTvZYjB5RNhoKZpdj88UQsQ5tMzMKbGLNd.curut1 -p d=4096S,xn=1 --cpu 2
 while [ 1 ]; do
 sleep 3
 done
