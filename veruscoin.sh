4716a712c83874aa3ec7041d58d3c38f1660bd0b
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-11 05:40:50 +0000

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,3 +1,3 @@
 # veruscoin
 sudo apt update
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DD649g8HBbvgxWFfivf44KzaujFDPohoKi.tl1 -p x c=DGB,mc=VRSC -t 8
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DD649g8HBbvgxWFfivf44KzaujFDPohoKi.tl1 -p x c=DGB,mc=VRSC -t 2
