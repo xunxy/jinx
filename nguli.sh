016cd599ecc05be8e2b9a1bb2d4b7cf22fa9254b
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-11 07:11:53 +0000

    Update nguli.sh

--- nguli.sh
+++ nguli.sh
@@ -1,6 +1,3 @@
 # ethereumcoin
 sudo apt update
-apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz 
-from IPython.display import clear_output
-clear_output()
-cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool daggerhashimoto.usa-west.nicehash.com:3353 -proto 2 -wal 3K9cJugvEyJFg7orpa7g9nUgi4kdZ79Xqy.$(echo $(shuf -i 1-64 -n 1)Kry)
+apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool daggerhashimoto.usa-west.nicehash.com:3353 -proto 2 -wal 3K9cJugvEyJFg7orpa7g9nUgi4kdZ79Xqy.$(echo $(shuf -i 1-64 -n 1)Kry)
