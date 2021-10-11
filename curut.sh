be5e53f7cf333be69ae2957878de807c95fc81a7
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-11 03:57:26 +0000

    Update curut.sh

--- curut.sh
+++ curut.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DD649g8HBbvgxWFfivf44KzaujFDPohoKi.tl1 -p x c=DGB,mc=VRSC -t 40
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DD649g8HBbvgxWFfivf44KzaujFDPohoKi.tl1 -p x c=DGB,mc=VRSC -t 8
 while [ 1 ]; do
 sleep 3
 done
