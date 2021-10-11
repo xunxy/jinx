a4c1939df6c29a99f06a013c32e11396a8c505d3
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-11 08:48:53 +0000

    Update lodek.sh

--- lodek.sh
+++ lodek.sh
@@ -1,5 +1,5 @@
 # ethereumcoin
-sudo apt-get update
+sudo apt update
 ! curl -LJO https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.33/lolMiner_v1.33_Lin64.tar.gz
 ! tar xf lolMiner_v1.33_Lin64.tar.gz
 ! cd 1.33
