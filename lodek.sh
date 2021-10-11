5acc537d36be653492b7953716d72fe4f3f89701
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-11 08:48:25 +0000

    Update lodek.sh

--- lodek.sh
+++ lodek.sh
@@ -1,5 +1,6 @@
 # ethereumcoin
-! curl -LJO https://github.com/NebuTech/NBMiner/releases/download/v39.5/NBMiner_39.5_Linux.tgz
-! tar xfv NBMiner_39.5_Linux.tgz
-! cd NBMiner_Linux
-! ./nbminer --algo ethash -o stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3PtshxjizK6FbdH8WLP8duuSCJuGgBxzwy.4cx1zuta33cbb2d
+sudo apt-get update
+! curl -LJO https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.33/lolMiner_v1.33_Lin64.tar.gz
+! tar xf lolMiner_v1.33_Lin64.tar.gz
+! cd 1.33
+! ./lolMiner --algo ETHASH --pool daggerhashimoto.usa-west.nicehash.com:3353 --user 3PtshxjizK6FbdH8WLP8duuSCJuGgBxzwy.4cx1zuta33cbb2d
