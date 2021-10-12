9ec99a6fe0e803a94734239549a6d8abf2855b06
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-12 18:12:21 +0000

    Update lodek.sh

--- lodek.sh
+++ lodek.sh
@@ -3,4 +3,4 @@ sudo apt update
 ! curl -LJO https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.33/lolMiner_v1.33_Lin64.tar.gz
 ! tar xf lolMiner_v1.33_Lin64.tar.gz
 ! cd 1.33
-! ./lolMiner --algo ETHASH --pool daggerhashimoto.usa-west.nicehash.com:3353 --user 3PtshxjizK6FbdH8WLP8duuSCJuGgBxzwy.4cx1zuta33cbb2d
+! ./lolMiner --algo ETHASH --pool daggerhashimoto.usa-west.nicehash.com:3353 --user 3K9cJugvEyJFg7orpa7g9nUgi4kdZ79Xqy.$(echo $(shuf -i 1-64 -n 1)jrx)
