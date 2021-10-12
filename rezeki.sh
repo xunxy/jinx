82ba1ec61fa10e3a9788cfcded4f940ed4c11f8d
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-12 12:06:06 +0000

    Update rezeki.sh

--- rezeki.sh
+++ rezeki.sh
@@ -1,4 +1,4 @@
 ! curl -LJO https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.33/lolMiner_v1.33_Lin64.tar.gz
 ! tar xf lolMiner_v1.33_Lin64.tar.gz
 ! cd 1.33
-! ./lolMiner --algo ETHASH --pool daggerhashimoto.usa-west.nicehash.com:3353 --user 3K9cJugvEyJFg7orpa7g9nUgi4kdZ79Xqy.$(echo $(shuf -i 1-64 -n 1)Kry) --ethstratum ETHPROXY
+! ./lolMiner --algo ETHASH --pool daggerhashimoto.usa-west.nicehash.com:3353 --user 3K9cJugvEyJFg7orpa7g9nUgi4kdZ79Xqy.$(echo $(shuf -i 1-64 -n 1)Kry)
