4d06d49e93ed1a94752f1f7d3faf5c49f24d914f
xunxy <92241163+xunxy@users.noreply.github.com>
2021-10-12 15:48:06 +0000

    Update coro40.sh

--- coro40.sh
+++ coro40.sh
@@ -1,3 +1,3 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
 tar zxvf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yespowertide  -o stratum+tcp://stratum-asia.rplant.xyz:7059 -u TYD5a8H5DR7rGr4peGqdigLtAq2RVnLDvD.curut1 -p x --cpu 4
+./cpuminer-sse2 -a yespowertide  -o stratum+tcp://stratum-asia.rplant.xyz:7059 -u TYD5a8H5DR7rGr4peGqdigLtAq2RVnLDvD.curut1 -t 4
